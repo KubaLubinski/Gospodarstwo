48b1598081be6c322aa443463d035c6ba93b4f56
KubaLubinski <kubalubinski0@gmail.com>
2022-12-14 14:01:32 +0100

    Dodaj pliki projektów.

---
+++ Gospodarstwo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gospodarstwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
